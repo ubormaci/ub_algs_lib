b3e4d8ef2d2e8d324ae981040e615815a7679f0a
ubormaci <ubormaci@gmail.com>
2025-02-05 14:15:31 +0200

    Modified Khrushkal, properly stress tested

--- khrushkal.cpp
+++ khrushkal.cpp
@@ -1,62 +1,101 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include <map>
+#include <set>
+#include <cmath>
+#include <array>
+#include <string>
+#include <cstdio>
+#include <iterator>
+#include <unordered_set>
+#include <cstdint>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <numeric>
 #include <fstream>
+#include <bitset>
+#include <iomanip>
 using namespace std;
-     
+
+#pragma GCC optimize("O2")
+// #pragma GCC optimize("O1","O2","O3","Ofast","unroll-loops")
+// #pragma GCC target("sse","sse2","sse3","sse4.1","sse4.2","avx","avx2")
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#ifdef LOCAL
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+/*
+
+notes:
+
+int64_t
+stoi(string s) -> string to int
+to_string() -> int (or else) to string
+
+vector declaration:
+vector<ll> v(n, 0)
+vector<vector<ll>> v(n, vector<ll>(n, 0));
+
+{if statement} ? {truth value} : {false value}
+
+set lower bound/upper bound:
+ 	// . . . m1 . . . d . . . . m2
+    auto m1_it = b.lower_bound(d);
+    advance(m1_it, -1);
+    m1 = *m1_it;
+	m2 = *b.upper_bound(d);
+
+#ifdef LOCAL
+    freopen("in.txt","r",stdin);
+    freopen("out.txt","w",stdout);
+#endif
+
+constexpr auto lcm(auto x, auto... xs)
+{
+	return ((x = std::lcm(x, xs)), ...);
+}
+
+std::gcd(int a, int b)
+
+cout << setprecision(n);
+
+*/
+
+// Khruskal with "We have DSU at home" implementation of DSU
+// stress tested against CSES: Road Reparation
+
 typedef long long ll;
-     
-    /*
-     
-    https://www.pbinfo.ro/articole/19048/algoritmul-lui-kruskal
-     
-    input1:
-     
-    9 14
-    4 1 2
-    8 1 8
-    8 2 3
-    11 2 8
-    4 3 6
-    7 3 4
-    2 3 9
-    9 4 5
-    14 4 6
-    10 5 6
-    2 6 7
-    6 7 9
-    1 7 8
-    7 8 9
-     
-    output
-     
-    9 8
-    1 7 8
-    2 3 9
-    2 6 7
-    4 1 2
-    4 3 6
-    7 3 4
-    8 1 8
-    9 4 5
-     
-    */
-     
+
 int main()
 {
     vector < pair<ll, pair<ll, ll>>> gr;
     // length - from - to
      
-    ifstream cin("example.txt");
+    //ifstream cin("example.txt");
     ll n, m;
     cin >> n >> m;
     for (ll i = 0; i < m; i++) {
         ll weight, from, to;
-        cin >> weight >> from >> to;
+        cin >> from >> to >> weight;
         gr.push_back({ weight, {from, to} });
     }
-     
+
+	/* 
+	if(m < n - 1) {
+		cout << "IMPOSSIBLE";
+		return 0;
+	}
+	*/
+
     sort(gr.begin(), gr.end());
     // two vectors: one that stores the colors of each vertex
     // vertex -> color
@@ -72,7 +111,7 @@ int main()
      
     ll i = 0;
     ll currcol = 0;
-    while (keptctr < (n - 1)) {
+    while (i < m) {
         ll start = gr[i].second.first;
         ll end = gr[i].second.second;
         //ll vertexcolor[start] = vertexcolor[start];
@@ -80,12 +119,12 @@ int main()
     
         string debug = "\n      ";
     
-        cout << "\n";
-        cout << debug << "i=" << i;
-        cout << debug << "start = " << start;
-        cout << debug << "end=" << end;
-        cout << debug << "vertexcolor[start]=" << vertexcolor[start];
-        cout << debug << "vertexcolor[end]=" << vertexcolor[end];
+        //cout << "\n";
+        //cout << debug << "i=" << i;
+        //cout << debug << "start = " << start;
+        //cout << debug << "end=" << end;
+        //cout << debug << "vertexcolor[start]=" << vertexcolor[start];
+        //cout << debug << "vertexcolor[end]=" << vertexcolor[end];
     
         if (vertexcolor[start] == vertexcolor[end] && vertexcolor[start] != -1 && vertexcolor[end] != -1) {
             i++;
@@ -115,8 +154,8 @@ int main()
             // thus we have concluded that both have been colored, in different colors
             ll col1ctr = colors[vertexcolor[start]].size();
             ll col2ctr = colors[vertexcolor[end]].size();
-            cout << debug << "col1ctr=" << col1ctr;
-            cout << debug << "col2ctr=" << col2ctr;
+            //cout << debug << "col1ctr=" << col1ctr;
+            //cout << debug << "col2ctr=" << col2ctr;
             if (col1ctr > col2ctr) {
                 // ha tobb van az egyesbol mint a kettesbol
                 // akkor a kettest az egyesre szinezzuk
@@ -136,11 +175,12 @@ int main()
                 colors[prevstartcol].clear();
             }
         }
-        cout << debug << "vertexcolors: ";
-            for (ll i = 1; i <= n; i++) {
-            cout << i << ":" << vertexcolor[i] << " ; ";
-        }
-        cout << debug << "colored vertexes: ";
+        //cout << debug << "vertexcolors: ";
+            //for (ll i = 1; i <= n; i++) {
+            //cout << i << ":" << vertexcolor[i] << " ; ";
+        	//}
+        /*
+		//cout << debug << "colored vertexes: ";
         for (const auto& x : colors) {
             if (x.second.size() != 0) {
                 cout << debug << x.first << " ";
@@ -149,17 +189,31 @@ int main()
                 }
             }
         }
+		*/
     }
  
-    cout << "\nMin-fa:";
-    cout << "\n" << n << " " << n - 1;
+    //cout << "\nMin-fa:";
+    //cout << "\n" << n << " " << n - 1;
 
-     for (ll i = 0; i < m; i++) {
+	//cerr << "vcol=" << vertexcolor;
+
+	for(ll i = 1; i <= n; i++) {
+		if(vertexcolor[i] == -1 || vertexcolor[i] != vertexcolor[1]) {
+			cout << "IMPOSSIBLE";
+			return 0;
+		}
+	}
+
+	ll cost = 0;
+	
+    for(ll i = 0; i < m; i++) {
         if (kept[i] == true) {
-            ll start = gr[i].second.first;
-            ll end = gr[i].second.second;
-            cout << "\n" << gr[i].first << " " << start << " " << end;
+			cost += gr[i].first;
+            //ll start = gr[i].second.first;
+            //ll end = gr[i].second.second;
+            //cout << "\n" << gr[i].first << " " << start << " " << end;
         }
     }
+	cout << cost;
   
 }
